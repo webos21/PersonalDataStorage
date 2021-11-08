0f81eb30084b312fb86ed261ff9fd0d3413376c3
CheolMin Jo <webos21@gmail.com>
2021-11-08 14:15:24 +0900

    * RC v1.0.4  - refining the resources

---
+++ app/src/main/java/com/gmail/webos21/pds/app/SplashScreenActivity.java
@@ -0,0 +1,17 @@
+package com.gmail.webos21.pds.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        finish();
+    }
+
+}
