518a172b48f7c5d0322d7952a20d6cc00e38d7c4
Cheolmin Jo <webos21@gmail.com>
2021-12-23 18:41:13 +0900

    working on gradle update

--- app/src/main/java/com/gmail/webos21/pds/app/ui/webservice/WebserviceFragment.java
+++ app/src/main/java/com/gmail/webos21/pds/app/ui/webservice/WebserviceFragment.java
@@ -1,8 +1,14 @@
 package com.gmail.webos21.pds.app.ui.webservice;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
 import android.net.Uri;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -53,6 +59,7 @@ public class WebserviceFragment extends Fragment {
     private DirWebSwitchListener dwsl;
     private PbWebSwitchListener pwsl;
 
+    private String ipAddress;
     private File pwdir;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -105,6 +112,10 @@ public class WebserviceFragment extends Fragment {
         PdsApp app = (PdsApp) getActivity().getApplicationContext();
         PdsWebHelper pwh = app.getPdsWebHelper();
 
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+            requestIpAddress();
+        }
+
         swPbWeb.setOnCheckedChangeListener(null);
         if (pwh.isPlaying()) {
             String addr = pwh.getUri();
@@ -125,17 +136,46 @@ public class WebserviceFragment extends Fragment {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.S)
+    private void requestIpAddress() {
+        final NetworkRequest request =
+                new NetworkRequest.Builder()
+                        .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                        .build();
+        final ConnectivityManager connectivityManager =
+                getActivity().getSystemService(ConnectivityManager.class);
+        final ConnectivityManager.NetworkCallback networkCallback = new ConnectivityManager.NetworkCallback() {
+            @Override
+            public void onAvailable(Network network) {
+            }
+
+            @Override
+            public void onCapabilitiesChanged(Network network, NetworkCapabilities networkCapabilities) {
+                WifiInfo wifiInfo = (WifiInfo) networkCapabilities.getTransportInfo();
+                ByteBuffer bb = ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN);
+                byte[] hostAddr = bb.putInt(wifiInfo.getIpAddress()).array();
+                try {
+                    ipAddress = InetAddress.getByAddress(hostAddr).getHostAddress();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        connectivityManager.requestNetwork(request, networkCallback); // For request
+    }
+
     private String getIpAddress() {
-        WifiManager wm = (WifiManager) getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        String ip = null;
-        try {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+            WifiManager wm = (WifiManager) getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
             ByteBuffer bb = ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN);
             byte[] hostAddr = bb.putInt(wm.getConnectionInfo().getIpAddress()).array();
-            ip = InetAddress.getByAddress(hostAddr).getHostAddress();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
+            try {
+                ipAddress = InetAddress.getByAddress(hostAddr).getHostAddress();
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
         }
-        return ip;
+        return ipAddress;
     }
 
     @RequiresApi(Build.VERSION_CODES.R)
